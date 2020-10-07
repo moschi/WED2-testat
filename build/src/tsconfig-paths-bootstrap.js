3849daf6f6daae1a9af99df2ed11243570cbd35d
David Kühnhanss <david.kuehnhanss@gmail.com>
2020-10-07 13:08:01 +0200

    added typescript and prettier

---
+++ build/src/tsconfig-paths-bootstrap.js
@@ -0,0 +1,9 @@
+"use strict";
+/* eslint-disable @typescript-eslint/no-var-requires */
+var tsConfig = require('../tsconfig.json');
+var tsConfigPaths = require('tsconfig-paths');
+var baseUrl = './build';
+tsConfigPaths.register({
+    baseUrl: baseUrl,
+    paths: tsConfig.compilerOptions.paths,
+});
---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import createError = require('http-errors');
+import express = require('express');
+import path = require('path');
+import cookieParser = require('cookie-parser');
+import logger = require('morgan');
+import sassMiddleware = require('node-sass-middleware');
+import indexRouter = require('./routes/index');
+import usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true,
+  }),
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter as express.Application);
+app.use('/users', usersRouter as express.Application);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+app.use(() => (err: any, req: express.Request, res: express.Response) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export = app;
